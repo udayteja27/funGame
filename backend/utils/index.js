451ae85db6c2cb692d733bf8791727f14dee28e9
udayteja27 <udyteja.kut@gmail .com>
2024-03-17 20:12:11 +0530

    solution

--- backend/utils/index.js
+++ backend/utils/index.js
@@ -1,20 +1,20 @@
 const CHARATERS = [
-    "Cat card 😼",
-    "Defuse card 🙅‍♂️",
-    "Shuffle card 🔀",
-    "Exploding kitten card 💣",
-  ];
-  
-  const generateRandomCards = () => {
-    const randomDeck = [];
-    for (let i = 0; i < 5; i++) {
-      const index = Math.floor(Math.random() * CHARATERS,length);
-      //const index = 0; // temp
-      randomDeck.push(CHARATERS[index]);
-    }
-    return randomDeck;
-  };
-  
-  module.exports = {
-    generateRandomCards,
-  };
+  "Cat card 😼",
+  "Defuse card 🙅‍♂️",
+  "Shuffle card 🔀",
+  "Exploding kitten card 💣",
+];
+
+const generateRandomCards = () => {
+  const randomDeck = [];
+  for (let i = 0; i < 5; i++) {
+    // const index = Math.random() * 4;
+    const index = 0; // temp
+    randomDeck.push(CHARATERS[Math.floor(index)]);
+  }
+  return randomDeck;
+};
+
+module.exports = {
+  generateRandomCards,
+};
